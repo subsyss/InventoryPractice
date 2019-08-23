17ec2bb5a27aaf8f3c78434307ffc8ba5505c9b8
subsyss <choe.byeonghyeon@gmail.com>
2019-08-23 20:38:54 +0900

    first

---
+++ Assets/Scripts/Inventory Framework/UI/UI_Slot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SA.InventoryFramework
+{
+    public class UI_Slot : MonoBehaviour
+    {
+        public void OnClick(UI_InventoryManager invManager)
+        {
+            Debug.Log("on click");
+        }
+
+        public void OnDropItem(UI_InventoryManager invManager)
+        {
+            Debug.Log("drop item");
+
+        }
+
+    }
+}
